6ea4acb5e184c220b96bf37db5f864758d061118
cnwarden <cnwarden@gmail.com>
2015-05-02 10:20:46 +0800

    optimize query with exclude

--- bijia/views.py
+++ bijia/views.py
@@ -22,25 +22,25 @@ def stocklistview(request):
     if provider == 'jd':
         if display_method == '1':
             #我看你最值
-            stocks = Stock.objects.exclude('price_list').filter(category=category).order_by('-degree.value')[0:20]
+            stocks = Stock.objects.exclude('price_list').exclude('mobile_price_list').filter(category=category).order_by('-degree.value')[0:20]
         elif display_method == '2':
             #所有商品
-            stocks = Stock.objects.filter(category=category).order_by('-last_price')[0:20]
+            stocks = Stock.objects.exclude('price_list').exclude('mobile_price_list').filter(category=category).order_by('-last_price')[0:20]
         elif display_method == '3':
             #本日上新
             today = datetime.now().date()
             midnight = datetime(today.year, today.month, today.day)
-            stocks = Stock.objects.filter(category=category).filter(create_time__gte=midnight).order_by('-create_time')[0:20]
+            stocks = Stock.objects.exclude('price_list').exclude('mobile_price_list').filter(category=category).filter(create_time__gte=midnight).order_by('-create_time')[0:20]
             pass
         elif display_method == '4':
             #最新价格变动
-            stocks = Stock.objects.filter(category=category).order_by('-degree.change_time')[0:20]
+            stocks = Stock.objects.exclude('price_list').exclude('mobile_price_list').filter(category=category).order_by('-degree.change_time')[0:20]
         elif display_method == '5':
             #移动好价
-            stocks = Stock.objects.filter(category=category).where('this.last_price != this.last_mobile_price').order_by('-degree.change_time')[0:20]
+            stocks = Stock.objects.exclude('price_list').exclude('mobile_price_list').filter(category=category).where('this.last_price != this.last_mobile_price').order_by('-degree.change_time')[0:20]
         else:
             #所有商品
-            stocks = Stock.objects.all().order_by('-comments')[0:20]
+            stocks = Stock.objects.exclude('price_list').exclude('mobile_price_list').order_by('-comments')[0:20]
 
         return render_to_response('stock_list.html',
                                   context={'stock_list' : stocks,
