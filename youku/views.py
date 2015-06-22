c60e7ee297e2b04c91d0d3740aa4e87738b6f832
cnwarden <cnwarden@gmail.com>
2015-06-22 10:08:40 +0800

    + bug fixed, class variables

--- youku/views.py
+++ youku/views.py
@@ -14,9 +14,8 @@ def youku_list(request):
     content = ""
     yk = YouKu()
     yk.getList(url)
-    content = "<h1>%s</h1>" % (yk.name)
+    content = "<h1><a href=%s>%s</a></h1>" % (yk.url, yk.name)
     content += "<h2>%d</h2>" % (yk.videos.__len__())
     for item in yk.videos:
-        content = content + "%02d;%s<br/>" % (int(item[0]),item[1])
-    yk.clean()
+        content = content + "%02d;<a href=%s>%s</a><br/>" % (int(item[0]),item[1], item[1])
     return HttpResponse(content)
--- youku/youku.py
+++ youku/youku.py
@@ -10,17 +10,22 @@ import re
 
 class YouKu(object):
 
-    name = ""
-    siteId = 0
-    videos = []
-    available = False
-
     def __int__(self):
         super(self,YouKu).__init__()
+        self.url = None
+        self.name = None
+        self.videos = None
 
 
     def getList(self, url):
-        response = urllib2.urlopen(url)
+        self.url = url
+        m = re.search("siteId=14$", self.url)
+        if m:
+            pass
+        else:
+            self.url = "%s?siteId=14" % (self.url)
+
+        response = urllib2.urlopen(self.url)
         content = response.read()
         bs = BeautifulSoup(content)
         #linkpanels site14
@@ -28,23 +33,18 @@ class YouKu(object):
         self.name = video_name.h1.getText()
         video_div = bs.find("div", {"class":"linkpanels site14"})
         video_ul = video_div.ul
+        self.videos = []
         for video_li in video_ul.findAll("li"):
             video_name = video_li.a.getText()
             video_url = video_li.a['href']
-            self.videos.append((video_name, video_url))
-
-    def clean(self):
-        self.videos = []
+            if video_name and video_url:
+                self.videos.append((video_name, video_url))
 
 
-
-
-if __name__ == "__main__":
-    pass
-#    youku = YouKu()
-#    print dir(youku)
-#    youku.getList("http://www.soku.com/detail/show/XMTAzNjc5Ng==")
-#    for item in youku.videos:
-#        print item[1]
+if __name__ == '__main__':
+    youku = YouKu()
+    youku.getList("http://www.soku.com/detail/show/XMTAzNjc5Ng==")
+    for item in youku.videos:
+        print item[1]
 
 
